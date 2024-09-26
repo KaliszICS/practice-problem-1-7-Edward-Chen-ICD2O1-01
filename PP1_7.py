1b0d5270094a753725c44280390807ac496063b5
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-09-26 13:23:57 +0000

    2321

--- PP1_7.py
+++ PP1_7.py
@@ -1,20 +1,39 @@
 
 
-q1():
+def q1():
   #Write code here
-
-q2():
+  truebool1=True
+  print(truebool1)
+def q2():
   #Write code here
-
-q3():
+  num=input("Input an integer: ")
+  num1= 5
+  num1= int(num1)
+  num= int(num)
+  bool= num1 < num
+  print (bool)
+def q3():
   #Write code here
-
-q4():
+  letter=input("Input the letter a: ")
+  bool= (letter =="a")
+  print(bool)
+def q4():
   #Write code here
-
-q5():
+  word=input("Input a word earlier in the dictionary than google: ")
+  bool= (word<"google")
+  print(bool)
+def q5():
   #Write code here
-
+  number=input("Input an integer: ")
+  number2=input("Input another integer: ")
+  number=int(number)
+  number2=int(number2)
+  number3=int(number*number2)
+  number4=40
+  number4=int(number4)
+  bool=number3>number4
+  print(f"Number {number}*{number2}  is: {number3}")
+  print(bool)
 #Do edit the code below
 #Comment the lines below when running your tests
 
